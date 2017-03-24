3a9674d00b2c57beaad510199577cc5cecbfd372
Смирнов Владимир Юрьевич <iv622s15@lwp05.csc.local>
2017-03-24 14:47:00 +0700

    Input

---
+++ l2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int m, d;
+	printf("Ваш вклад:");
+	scanf("%d",&m);
+	printf("Кол-во дней:");
+	scanf("%d",&d);
+	return 0;
+}
